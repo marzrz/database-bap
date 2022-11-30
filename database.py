fd2b59d69d02522f8d695555ebfa8511c01b9f15
Marina Ruiz <marinaruizrodriguez01@gmail.com>
2022-11-30 18:07:38 +0100

    Init

--- database.py
+++ database.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
       context = ssl.SSLContext()
       context.load_cert_chain("/etc/ssl/certs/conversational_ugr_es.pem","/etc/ssl/certs/conversational_ugr_es.key")
       CORS(app)
-      app.run(host='0.0.0.0',port=5100,ssl_context=context,debug=False)
+      app.run(host='0.0.0.0',port=5200,ssl_context=context,debug=False)
