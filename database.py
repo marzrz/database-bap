55bc94ac1ea8877909b618766e1a25e590ebcfab
Marina Ruiz <marinaruizrodriguez01@gmail.com>
2022-12-01 17:36:36 +0100

    User exists

--- database.py
+++ database.py
@@ -14,8 +14,8 @@ def userExists():
       username = request.json["username"]
       password = request.json["password"]
       userDocument = mongo.db.user.find_one({"username": username, "password": password})
-      user = json_util.dumps(userDocument)
-      print (user['_id']['$oid'])
+      user = jsonify(userDocument)
+      print (user['_id'])
       
       if (userDocument):
             data = {
